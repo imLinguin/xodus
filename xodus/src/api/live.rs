d39f5db64b82b9c7f94b66d8b16046a9a03dba34
Christopher Homberger <christopher.homberger@web.de>
2026-06-10 17:13:07 +0200

    remove broken code

--- xodus/src/api/live.rs
+++ xodus/src/api/live.rs
@@ -40,55 +40,6 @@ pub async fn login_device_credential(
     Ok(resp)
 }
 
-pub async fn authenticate_apppwd(
-    client: &reqwest::Client,
-    username: String,
-    password: String,
-) -> reqwest::Result<soap::Envelope> {
-    let mut header = soap::Header::new();
-    header.security.username_token = Some(UsernameToken{
-        id: "user".to_string(),
-        username: username,
-        password: password,
-        login_option: "".to_string(),
-        username_hint: "".to_string(),
-    });
-    let body = soap::Body {
-        body: soap::BodyContent::RequestSecurityToken(soap::RequestSecurityToken {
-            id: "RST0".to_string(),
-            request_type: "http://schemas.xmlsoap.org/ws/2005/02/trust/Issue".to_string(),
-            applies_to: AppliesTo {
-                endpoint_reference: EndpointReference {
-                    address: "http://Passport.NET/tb".to_string(),
-                },
-            },
-            policy_reference: Some(PolicyReference {
-                uri: "TOKEN_BROKER".to_string(),
-                val: "scope=service::www.microsoft.com::mbi_ssl&amp;uaid=FCFF621D-6BE5-4F41-9B69-68B6B2563B35&amp;clientid=%7Bf0c62012-2cef-4831-b1f7-930682874c86%7D&amp;ssoappgroup=none".to_string()
-            }),
-        }),
-    };
-    let envelope = soap::Envelope::new(header, body);
-    let xml = quick_xml::se::to_string(&envelope).unwrap();
-    let xml = format!("{XML_HEADER}\n{xml}");
-    let response = client
-        .post(format!(
-            "https://login.live.com/RST2.srf"
-        ))
-        .header("User-Agent", "MSAWindows/55 (OS 10.0.26100.0.0 ge_release; IDK 10.0.26100.5074 ge_release; Cfg 16.000.29325.00; Test 0)")
-        .header("Content-Type", "application/soap+xml")
-        .header("Host", "login.live.com")
-        .body(xml)
-        .send()
-        .await?;
-
-    let text = response.text().await?;
-    let res_envelope: soap::Envelope = quick_xml::de::from_str(&text).expect("Failed to de xml");
-
-    Ok(res_envelope)
-}
-
-
 pub async fn authenticate_device(
     client: &reqwest::Client,
     username: String,
