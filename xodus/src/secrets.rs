cada2b81c8f374b8cdb94f08e97f27be370efc49
Christopher Homberger <christopher.homberger@web.de>
2026-06-13 18:01:22 +0200

    fixes

--- xodus/src/secrets.rs
+++ xodus/src/secrets.rs
@@ -1,5 +1,3 @@
-#[cfg(target_os = "macos")]
-use std::path::PathBuf;
 
 pub static SERVICE_NAME: &str = "Xodus Service";
 
@@ -12,7 +10,7 @@ pub fn init_secrets() -> Result<(), keyring_core::Error> {
 
     #[cfg(target_os = "macos")]
     {
-        keyring_core::set_default_store(apple_native_keyring_store::keyring::Store::new()?);
+        keyring_core::set_default_store(apple_native_keyring_store::keychain::Store::new()?);
         return Ok(());
     }
 
@@ -32,12 +30,4 @@ pub fn get_entry(user: &str) -> Result<keyring_core::Entry, keyring_core::Error>
 
 pub fn destroy_secrets() {
     keyring_core::unset_default_store();
-}
-
-#[cfg(target_os = "macos")]
-fn secrets_backing_file() -> PathBuf {
-    std::env::var_os("HOME")
-        .map(PathBuf::from)
-        .unwrap_or_else(std::env::temp_dir)
-        .join(".xodus-keyring.ron")
 }
